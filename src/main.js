3bca12928ddb9ebdb2ef2ac6570a9701a80b6a20
Vladimir Krivosheev <vladimir.krivosheev@jetbrains.com>
2015-05-02 20:58:53 +0200

    log ids

--- src/main.js
+++ src/main.js
@@ -1,7 +1,14 @@
 var connect = require("connect")
-var http = require("http")
-
 var oauthShim = require("oauth-shim")
+
+var oauthShimConfig = JSON.parse(process.env.OAUTH)
+if (oauthShimConfig == null) {
+  throw new Error("You must define OAUTH environment variable to specify client ID's and secret (see https://www.npmjs.com/package/oauth-shim)")
+}
+
+console.log("Client IDs", Object.keys(oauthShimConfig))
+
+oauthShim.init(oauthShimConfig)
 
 var app = connect()
 app.use(oauthShim)
